e0077fecc08ea41d7bf69cb3bfa073f525772f8d
drummeric <Wolfson.Eric@gmail.com>
2014-02-19 14:28:17 -0500

    Fixed issue with add pop-up and ShoppingListFragment

--- src/com/thundersnacks/virtualpantry/ShoppingListFragment.java
+++ src/com/thundersnacks/virtualpantry/ShoppingListFragment.java
@@ -32,11 +32,6 @@ public class ShoppingListFragment extends Fragment implements OnItemClickListene
 	private ListView mainListView = null;
 	String[] foodString;
 	// Called when the Fragment is attached to its parent Activity.
-	  @Override
-	  public void onAttach(Activity activity) {
-	    super.onAttach(activity);
-	    // Get a reference to the parent Activity.
-	  }
 	  //@Override
 		//public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
 		//	// TODO Auto-generated method stub
@@ -109,86 +104,5 @@ public class ShoppingListFragment extends Fragment implements OnItemClickListene
         	}
         });
         return view;
-    }
-	
-	@Override
-	public void onViewStateRestored (Bundle savedInstanceState)
-	{
-	}
-
-
-    
-    // Called once the parent Activity and the Fragment's UI have 
-    // been created.
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-      super.onActivityCreated(savedInstanceState);
-      // Complete the Fragment initialization particularly anything
-      // that requires the parent Activity to be initialized or the 
-      // Fragment's view to be fully inflated.
-    }
-
-    // Called at the start of the visible lifetime.
-    @Override
-    public void onStart(){
-      super.onStart();
-      // Apply any required UI change now that the Fragment is visible.
-    }    
-    
-    @Override
-    public void onResume(){
-      super.onResume();
-      // Resume any paused UI updates, threads, or processes required
-      // by the Fragment but suspended when it became inactive.
-    }
-        
- // Called at the end of the active lifetime.
-    @Override
-    public void onPause(){
-      // Suspend UI updates, threads, or CPU intensive processes
-      // that don't need to be updated when the Activity isn't
-      // the active foreground activity.
-      // Persist all edits or state changes
-      // as after this call the process is likely to be killed.
-      super.onPause();
-    }
-
-    // Called to save UI state changes at the
-    // end of the active lifecycle.
-    @Override
-    public void onSaveInstanceState(Bundle savedInstanceState) {
-      // Save UI state changes to the savedInstanceState.
-      // This bundle will be passed to onCreate, onCreateView, and
-      // onCreateView if the parent Activity is killed and restarted.
-      super.onSaveInstanceState(savedInstanceState);
-    }
-
-    // Called at the end of the visible lifetime.
-    @Override
-    public void onStop(){
-      // Suspend remaining UI updates, threads, or processing
-      // that aren't required when the Fragment isn't visible.
-      super.onStop();
-    }
-
-    // Called when the Fragment's View has been detached.
-    @Override
-    public void onDestroyView() {
-      // Clean up resources related to the View.
-      super.onDestroyView();
-    }
-
-    // Called at the end of the full lifetime.
-    @Override
-    public void onDestroy(){
-      // Clean up any resources including ending threads,
-      // closing database connections etc.
-      super.onDestroy();
-    }
-
-    // Called when the Fragment has been detached from its parent Activity.
-    @Override
-    public void onDetach() {
-      super.onDetach();
     }
 }
