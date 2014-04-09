ab4992965b4bf93dcd149e9c13f43895a013ee82
mikey <mikey@10.136.88.174>
2014-04-09 12:24:23 -0400

    Search Interface Done

--- src/com/thundersnacks/virtualpantry/MainActivity.java
+++ src/com/thundersnacks/virtualpantry/MainActivity.java
@@ -40,6 +40,7 @@ import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.SearchManager;
 import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
@@ -185,7 +186,12 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+               (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(
+        		searchManager.getSearchableInfo(getComponentName()));
         searchView.setQueryHint("Search Pantry");
         this.menu = menu;
         firstOpen = true;
