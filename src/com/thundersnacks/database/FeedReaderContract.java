eae8fd515972f75aa5d7254c5c3bda95d87a8454
sararose <sara.rose.schuster@gmail.com>
2014-04-11 13:22:04 -0400

    added database package

---
+++ src/com/thundersnacks/database/FeedReaderContract.java
@@ -0,0 +1,18 @@
+package com.thundersnacks.database;
+
+import android.provider.BaseColumns;
+
+public final class FeedReaderContract {
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    public FeedReaderContract() {}
+
+    /* Inner class that defines the table contents */
+    public static abstract class FeedEntry implements BaseColumns {
+        public static final String TABLE_NAME = "entry";
+        public static final String COLUMN_NAME_ENTRY_ID = "entryid";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_SUBTITLE = "subtitle";
+        // ...
+    }
+}
